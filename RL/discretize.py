cb112c5c8e33453004705707a0ab6faea8febf5a
ritabt <ritabt@stanford.edu>
2021-11-10 15:29:00 -0800

    get idx for all state vals

--- RL/discretize.py
+++ RL/discretize.py
@@ -21,16 +21,16 @@ class DiscretePos():
 		lower = bin_idx * step_size
 		higher = (bin_idx+1) * step_size
 		if abs(pos-lower) <= abs(pos-higher):
-			return bin_idx, lower
+			return bin_idx
 		else:
-			return bin_idx + 1, higher
+			return bin_idx + 1
 
 	def map_to_idx(self, idx_x, idx_y):
-		return idx_x + idx_y * self.width_bins
+		return idx_x + idx_y * (self.width_bins+1)
 
 	def discretize(self, pos_x, pos_y):
-		idx_x, pos_x = self.snap_to_grid(pos_x, self.world_width, self.width_bins)
-		idx_y, pos_y = self.snap_to_grid(pos_y, self.world_height, self.height_bins)
+		idx_x = self.snap_to_grid(pos_x, self.world_width, self.width_bins)
+		idx_y = self.snap_to_grid(pos_y, self.world_height, self.height_bins)
 		output = self.map_to_idx(idx_x, idx_y)
 		return output
 
@@ -40,6 +40,10 @@ class DiscreteHeading():
 		self.low = low
 		self.high = high
 		self.step_size = float((high - low)/num_bins)
+		self.vals = np.linspace(low, high, num=num_bins+1)
+
+	def get_val(self, idx):
+		return self.vals[idx]
 
 	def discretize(self, alpha):
 		alpha = alpha%(2*np.pi)
@@ -48,9 +52,9 @@ class DiscreteHeading():
 		higher = (bin_idx+1) * self.step_size
 
 		if abs(alpha-lower) <= abs(alpha-higher):
-			return lower
+			return bin_idx
 		else:
-			return higher
+			return bin_idx+1
 
 class DiscreteAction():
 	def __init__(self, low, high, num_bins):
@@ -58,6 +62,10 @@ class DiscreteAction():
 		self.high = high
 		self.num_bins = num_bins
 		self.step_size = float((high - low)/num_bins)
+		self.vals = np.linspace(low, high, num=num_bins+1)
+
+	def get_val(self, idx):
+		return self.vals[idx]
 
 	def discretize(self, val):
 		bin_idx = val//self.step_size
@@ -65,6 +73,6 @@ class DiscreteAction():
 		higher = (bin_idx+1) * self.step_size
 
 		if abs(val-lower) <= abs(val-higher):
-			return lower
+			return bin_idx
 		else:
-			return higher
+			return bin_idx+1
